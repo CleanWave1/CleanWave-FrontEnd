8971307df021a6b9624e0513901b14955ec115aa
Andrea <armix182002@gmail.com>
2024-05-02 23:22:28 -0500

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavbarContent from "@/public/navbar/navbar-content.component.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <navbar-content></navbar-content>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
-
+/*
 @media (min-width: 1024px) {
   header {
     display: flex;
@@ -44,4 +37,6 @@ header {
     flex-wrap: wrap;
   }
 }
+
+ */
 </style>
