f4095263d85aed54d3b804c44fbf2d2f0a8b62ff
CellBuuZer <leonardosolis752@gmail.com>
2024-05-04 01:17:26 -0500

    Add recovery components

---
+++ src/components/password-recovery-component.vue
@@ -0,0 +1,81 @@
+<script>
+
+export default {
+  name: 'RecoveryOptions',
+};
+
+</script>
+
+<template>
+  <div>
+    <div class="back-icon">
+      <a href="/login">
+        <span class="pi pi-arrow-left"></span> <!-- PrimeIcons back icon -->
+      </a>
+    </div>
+    <div class="card flex justify-content-center">
+      <img src="@/assets/cleanwave-logo.png" alt="logo-cleanwave" class="separator-image" />
+      <h2>¿Cómo desea recuperar su contraseña?</h2> <!-- Title -->
+      <div class="recovery-options">
+        <div class="checkbox-item">
+          <input type="checkbox" id="emailCheckbox" name="recoveryOption" value="email">
+          <label for="emailCheckbox" class="checkbox-label">Correo Electrónico</label>
+        </div>
+        <div class="checkbox-item">
+          <input type="checkbox" id="phoneCheckbox" name="recoveryOption" value="phone">
+          <label for="phoneCheckbox" class="checkbox-label">Número de Teléfono</label>
+        </div>
+      </div>
+      <div class="button-container">
+        <pv-button label="Siguiente"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.flex{
+  display:flex;
+  padding: 20px;
+}
+
+.back-icon {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+
+.button-container {
+  display: flex;
+  padding: 20px;
+  margin-left: 270px
+}
+
+.card{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
+  gap: 15px;
+}
+
+.separator-image {
+  width: 150px;
+  height: auto;
+  margin: 100px 250px 0;
+}
+
+.checkbox-label {
+  font-size: 18px;
+}
+
+.checkbox-item {
+  margin-bottom: 30px;
+}
+
+.recovery-options input[type="checkbox"] {
+  font-size: 18px;
+}
+
+</style>
---
+++ src/components/password-recovery-email-component.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  name: 'RecoveryPasswordviaEmail',
+};
+</script>
+
+<template>
+  <div>
+    <div class="back-icon">
+      <a href="/login">
+        <span class="pi pi-arrow-left"></span>
+      </a>
+    </div>
+    <div class="card flex justify-content-center">
+      <img src="@/assets/cleanwave-logo.png" alt="logo-cleanwave" class="separator-image" />
+      <h2>¡Revisa tu Correo Electrónico!</h2>
+      <div>
+        <p>Te enviamos un correo con las instrucciones para recuperar tu contraseña</p>
+      </div>
+
+      <div class="button-container">
+        <pv-button label="Listo"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.flex{
+  display:flex;
+  padding: 20px;
+}
+
+.back-icon {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+
+.button-container {
+  display: flex;
+  padding: 20px;
+  margin-left: 270px
+}
+
+.card{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
+  gap: 15px;
+}
+
+.separator-image {
+  width: 150px;
+  height: auto;
+  margin: 100px 250px 0;
+}
+
+.checkbox-label {
+  font-size: 18px;
+}
+
+.checkbox-item {
+  margin-bottom: 30px;
+}
+
+.recovery-options input[type="checkbox"] {
+  font-size: 18px;
+}
+
+</style>
