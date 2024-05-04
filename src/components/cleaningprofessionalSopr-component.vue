c55d48e0dd556dd67f47b562ac167623095ee9d1
CellBuuZer <leonardosolis752@gmail.com>
2024-05-04 11:35:48 -0500

    Add fixes for firebase deployment

---
+++ src/components/cleaningprofessionalSopr-component.vue
@@ -0,0 +1,99 @@
+<script>
+import NavbarContent from "@/public/navbar/navbar-content.component.vue"
+import { ref } from 'vue';
+
+export default {
+  name: "support",
+  components: {NavbarContent}
+}
+
+const consulta = ref(null);
+</script>
+
+<template>
+  <NavbarContent ></NavbarContent>
+  <div class="support-content">
+    <br><br><br>
+    <h1 class="title">Soporte al cliente</h1>
+    <br><br><br>
+    <div class="card">
+      <pv-accordion :activeIndex="0" class="accordion">
+        <pv-accordionTab header="¿Cómo usar la aplicación?" class="accordion-tab">
+          Contenido del primer panel
+        </pv-accordionTab>
+        <pv-accordionTab header="¿Qué se incluye en mi suscripción?" class="accordion-tab">
+          Contenido del segundo panel
+        </pv-accordionTab>
+        <pv-accordionTab header="¿Cómo puedo programar servicios de limpieza?" class="accordion-tab">
+          Contenido del tercer panel
+        </pv-accordionTab>
+        <pv-accordionTab header="¿Está mi información personal segura?" class="accordion-tab">
+          Contenido del cuarto panel
+        </pv-accordionTab>
+        <pv-accordionTab header="¿Cómo me mantengo en contacto con la aplicación?" class="accordion-tab">
+          Contenido del quinto panel
+        </pv-accordionTab>
+      </pv-accordion>
+    </div>
+  </div>
+
+  <div class="floatlabel">
+    <br><br><br>
+    <h2 class="title">Formulario de consulta</h2>
+    <div class="input-container">
+      <pv-float-label>
+        <textarea id="consulta" v-model="value" style="width: 100%; height: 100px"></textarea>
+        <label for="consulta">Escriba su consulta</label>
+      </pv-float-label>
+    </div>
+    <div class="button-container">
+      <pv-button label="Enviar" style="width: 200px; height: 50px; margin-top: 20px;" />
+    </div>
+  </div>
+</template>
+
+<style>
+.support-content {
+  margin-left: 150px;
+  margin-right: 150px;
+}
+
+.title {
+  font-size: 30px;
+}
+
+.card {
+  font-size: 18px;
+}
+
+.floatlabel {
+  margin-top: 50px;
+  margin-left: 150px;
+  margin-right: 150px;
+}
+
+.input-container {
+  margin-bottom: 20px;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+
+.accordion {
+  width: 100%;
+  max-width: 800px;
+}
+
+.accordion-tab {
+  padding: 20px;
+  font-size: 20px;
+}
+</style>
+
+
+
+
+
+
---
+++ src/components/forms-component.vue
@@ -0,0 +1,111 @@
+<script>
+import NavbarContent from "@/public/navbar/navbar-content.component.vue"
+
+export default {
+  name: "fill-details",
+  components: {NavbarContent}
+}
+
+import { ref } from 'vue';
+
+const tipoPropiedad = ref(null);
+const tipoLimpieza = ref(null);
+const tamañoEspacio = ref(null);
+const numHabitaciones = ref(null);
+const tipoSuelo = ref(null);
+const instrucciones = ref(null);
+</script>
+
+<template>
+  <NavbarContent ></NavbarContent>
+  <div class="container">
+    <div class="image-container">
+      <img src="@/assets/imagen-registrarespacio.jpg" alt="Image"/>
+    </div>
+    <div class="form-container">
+      <h1>Registrar detalles de mi espacio</h1>
+      <pv-float-label>
+        <input type="text" id="tipoPropiedad" v-model="tipoPropiedad" style="width: 330px" />
+        <label for="tipoPropiedad">Tipo de Propiedad</label>
+      </pv-float-label>
+      <br>
+      <pv-float-label>
+        <input type="text" id="tipoLimpieza" v-model="tipoLimpieza" style="width: 330px" />
+        <label for="tipoLimpieza">Tipo de Limpieza</label>
+      </pv-float-label>
+      <br>
+      <pv-float-label>
+        <input type="text" id="tamañoEspacio" v-model="tamañoEspacio" style="width: 330px"/>
+        <label for="tamañoEspacio">Tamaño del espacio</label>
+      </pv-float-label>
+      <br>
+      <pv-float-label>
+        <input type="text" id="numHabitaciones" v-model="numHabitaciones" style="width: 330px"/>
+        <label for="numHabitaciones">Número de habitaciones</label>
+      </pv-float-label>
+      <br>
+      <pv-float-label>
+        <input type="text" id="tipoSuelo" v-model="tipoSuelo" style="width: 330px"/>
+        <label for="tipoSuelo">Tipo de suelo</label>
+      </pv-float-label>
+      <br>
+      <pv-float-label>
+        <textarea id="instrucciones" v-model="instrucciones" style="width: 330px; height: 80px"></textarea>
+        <label for="instrucciones">Instrucciones</label>
+      </pv-float-label>
+      <br>
+      <div class="button">
+        <router-link to="/inicio" tag="button" class="btn-primary">
+          <i class="pi pi-check"></i> Guardar
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+}
+
+.image-container {
+  margin-right: 50px;
+}
+
+.form-container {
+  width: 50%;
+}
+
+.button {
+  margin-top: 20px;
+}
+
+.btn-primary {
+  background-color: green;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.btn-primary:hover {
+  background-color: darkgreen;
+}
+
+input[type="text"],
+textarea {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 8px;
+  box-sizing: border-box;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+label {
+  margin-bottom: 5px;
+  display: block;
+}
+</style>
